f48021cb54dc977689e3b1d433e1d4fc4ae88f3c
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-06-01 21:45:43 +0900

    initiate new project

---
+++ src/main/java/com/chat/toktalk/ToktalkApplication.java
@@ -0,0 +1,12 @@
+package com.chat.toktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToktalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToktalkApplication.class, args);
+	}
+}
