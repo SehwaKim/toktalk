03560d506a924edb9f7ada38a0c2d982c1e0efc3
김세화 <sehwakm@gmail.com>
2018-06-02 20:12:16 +0900

    add: 유저관련 controller, service, repository 추가 및 시큐리티 설정

---
+++ src/main/java/com/chat/toktalk/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.chat.toktalk.service.impl;
+
+import com.chat.toktalk.domain.User;
+import com.chat.toktalk.repository.UserRepository;
+import com.chat.toktalk.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User getUserByEmail(String email) {
+
+        return userRepository.findUsersByEmail(email);
+    }
+
+    @Override
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into user (id, email, password, nickname, regdate) VALUES (1, 'test', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '아이스베어', now());
+insert into user (id, email, password, nickname, regdate) VALUES (2, 'test2', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '갈색곰', now());
+insert into user (id, email, password, nickname, regdate) VALUES (3, 'test3', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '판다', now());
+
+insert into user_roles(id, role_name, user_id) VALUES (1, 'USER', 1);
+insert into user_roles(id, role_name, user_id) VALUES (2, 'ADMIN', 1);
+insert into user_roles(id, role_name, user_id) VALUES (3, 'USER', 2);
+insert into user_roles(id, role_name, user_id) VALUES (4, 'ADMIN', 2);
+insert into user_roles(id, role_name, user_id) VALUES (5, 'USER', 3);
+
+insert into channel (id, name, regdate) VALUES (1, 'room number 1', now());
+insert into channel (id, name, regdate) VALUES (2, 'room number 2', now());
+insert into channel (id, name, regdate) VALUES (3, 'room number 3', now());
+insert into channel (id, name, regdate) VALUES (4, 'room number 4', now());
