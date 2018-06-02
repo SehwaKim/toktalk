2d55a1a47e09b6b281b4b59d9e933c09dfb1cc59
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-06-02 20:57:39 +0900

    add: RabbitMQ 추가

---
+++ src/main/java/com/chat/toktalk/config/RabbitConfig.java
@@ -0,0 +1,25 @@
+package com.chat.toktalk.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    public static final String QUEUE_NAME ="fanout_queue";
+    public static final String EXCHANGE_NAME="fanout_exchange";
+    @Bean
+     Queue messageQueue(){
+        return QueueBuilder.durable(QUEUE_NAME).build() ;
+    }
+    @Bean
+    FanoutExchange fanoutExchange(){
+        return new FanoutExchange(EXCHANGE_NAME,true,false);
+    }
+
+    @Bean
+    Binding binding(Queue messageQueue,FanoutExchange fanoutExchange){
+         return BindingBuilder.bind(messageQueue).to(fanoutExchange);
+    }
+
+}
---
+++ src/main/java/com/chat/toktalk/controller/MessageApiController.java
@@ -0,0 +1,25 @@
+package com.chat.toktalk.controller;
+
+import com.chat.toktalk.domain.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageApiController {
+    @Autowired
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageApiController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping
+    public String sendMessage(Message message){
+        rabbitTemplate.convertAndSend(message);
+
+        return "";
+    }
+
+}
