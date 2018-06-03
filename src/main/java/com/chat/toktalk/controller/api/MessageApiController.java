91fdddaf91be5086bbfd0c310838dbf64b065e60
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-06-03 16:08:41 +0900

    update: @PathVariable추가

--- src/main/java/com/chat/toktalk/controller/api/MessageApiController.java
+++ src/main/java/com/chat/toktalk/controller/api/MessageApiController.java
@@ -3,7 +3,9 @@ package com.chat.toktalk.controller.api;
 import com.chat.toktalk.domain.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +17,15 @@ public class MessageApiController {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-    @PostMapping
-    public String sendMessage(Message message){
+    @PostMapping(path = "/{channelId}/messages")
+    public String sendMessage(Message message, @PathVariable(value = "channelId")Long id){
         rabbitTemplate.convertAndSend(message);
+
+        //TODO
+        //RabbitListener구현(어디에서?)
+            //방정보로, Redis사용자들 Session 가져옴
+            //if(현재로컬사용자라면)
+                //session.sendMessage();(클라이언트로 메시지 전달)
 
         return "";
     }
