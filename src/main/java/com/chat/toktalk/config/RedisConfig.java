0b71fe9dbef533a200d201f92a83064bbe66d460
오세진 <jinioh88@gmail.com>
2018-06-04 13:09:47 +0900

    update : redis 관련 추가

--- src/main/java/com/chat/toktalk/config/RedisConfig.java
+++ src/main/java/com/chat/toktalk/config/RedisConfig.java
@@ -1,6 +1,6 @@
 package com.chat.toktalk.config;
 
-import io.lettuce.core.dynamic.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
