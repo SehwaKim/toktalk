8bb331e949a4f9c674682e59899fc48d7deeed3f
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-06-06 23:04:51 +0900

    add: MessageSender

---
+++ src/main/java/com/chat/toktalk/amqp/MessageSender.java
@@ -0,0 +1,17 @@
+package com.chat.toktalk.amqp;
+
+import com.chat.toktalk.config.RabbitConfig;
+import com.chat.toktalk.dto.ChatMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageSender {
+
+    @Autowired
+    private RabbitTemplate template;
+    public void sendMessage(ChatMessage chatMessage){
+        template.convertAndSend(RabbitConfig.EXCHANGE_NAME,"NO_ROUTING_KEY",chatMessage);
+    }
+}
