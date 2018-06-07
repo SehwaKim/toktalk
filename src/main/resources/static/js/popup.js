92c592f2b93546a3c3a21f4a49e8652cbc1dd398
김세화 <sehwakm@gmail.com>
2018-06-07 10:53:45 +0900

    add: 방생성 레이어팝업 추가

---
+++ src/main/resources/static/js/popup.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#btn-create").click(function () {
+        $("#createChannel").show();
+    });
+
+    $('#close').click(function () {
+        $('.pop-layer').hide();
+    })
+});
