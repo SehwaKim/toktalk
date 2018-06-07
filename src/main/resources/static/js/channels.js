c7824eac6c141a385f40a62e49cee338680b3b34
김세화 <sehwakm@gmail.com>
2018-06-07 21:19:52 +0900

    add: 서버로부터 heartbeat이 오면 pong 메세지 보내기 구현

--- src/main/resources/static/js/channels.js
+++ src/main/resources/static/js/channels.js
@@ -10,14 +10,21 @@ $(document).ready(function () {
     sock.onmessage = function (e) {
         var data = JSON.parse(e.data);
         var channelId = data.channelId;
-        if($("#"+channelId).css('display') != 'none'){
+        console.log(data);
+        showMessage(data);
+        /*if($("#"+channelId).css('display') != 'none'){
             showMessage(data);
-        }
+        }*/
     };
 
     sock.onclose = function () {
         console.log('disconnected');
     };
+
+    sock.onheartbeat = function () {
+        console.log("ping from server");
+        sock.send(JSON.stringify({'heartbeat' : 'pong'}));
+    }
 
     $("#chatInput_1").keypress(function(e) {
 
