f7f0306dbe0d4cca28f78edbc6f37b17bc8b4264
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-06-14 00:56:48 +0900

    add: UserOauthInfo

---
+++ src/main/java/com/chat/toktalk/domain/UserOauthInfo.java
@@ -0,0 +1,45 @@
+package com.chat.toktalk.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "user_oauth_info")
+public class UserOauthInfo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String accessToken;
+    String sub;
+    String name;
+    String picture;
+    String email;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    User user;
+
+    @Builder
+    public UserOauthInfo(String sub, String name, String picture, String email) {
+        this.sub = sub;
+        this.name = name;
+        this.picture = picture;
+        this.email = email;
+    }
+
+    public void setUser(User user){
+        this.user = user;
+        if(!user.getOauthInfos().contains(this)){
+            user.getOauthInfos().add(this);
+        }
+    }
+}
