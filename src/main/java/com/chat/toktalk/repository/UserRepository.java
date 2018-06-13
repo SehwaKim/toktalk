f78b88204710515da8ec22322cd14db6912cf7ac
DESKTOP-AH83DAJ\Serena-Laptop <noriming2@gmail.com>
2018-06-14 01:05:08 +0900

    update : oauth사용자의 email정보로 User정보 가져오는 쿼리작성

--- src/main/java/com/chat/toktalk/repository/UserRepository.java
+++ src/main/java/com/chat/toktalk/repository/UserRepository.java
@@ -2,7 +2,15 @@ package com.chat.toktalk.repository;
 
 import com.chat.toktalk.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     public User findUsersByEmail(String email);
+    public User findUserByEmail(String email);
+    @Query("SELECT oauth.user FROM UserOauthInfo oauth WHERE oauth.email = :email")
+    public User getOauthUser(@Param("email")String email);
+
+
 }
