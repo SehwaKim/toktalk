0eea668d50f4507b0bfa300b529f3a07609b38a4
김세화 <sehwakm@gmail.com>
2018-06-14 13:16:03 +0900

    충돌 해결

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,18 @@
 insert into user (id, email, password, nickname, regdate) VALUES (1, 'test', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '아이스베어', now());
 insert into user (id, email, password, nickname, regdate) VALUES (2, 'test2', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '갈색곰', now());
 insert into user (id, email, password, nickname, regdate) VALUES (3, 'test3', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '판다', now());
+insert into user (id, email, password, nickname, regdate) VALUES (4, 'bee', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '땡벌', now());
 
 insert into user_roles(id, role_name, user_id) VALUES (1, 'USER', 1);
 insert into user_roles(id, role_name, user_id) VALUES (2, 'ADMIN', 1);
 insert into user_roles(id, role_name, user_id) VALUES (3, 'USER', 2);
 insert into user_roles(id, role_name, user_id) VALUES (4, 'USER', 3);
+insert into user_roles(id, role_name, user_id) VALUES (5, 'USER', 4);
 
-insert into channel(id, name, regdate) VALUES (1, 'general', now());
-insert into channel_user(id, user_id, channel_id) VALUES (1, 1, 1);
-insert into channel_user(id, user_id, channel_id) VALUES (2, 2, 1);
+insert into channel(id, name, type, regdate) VALUES (1, 'general', 'public', now());
+insert into channel_user(id, user_id, channel_id, regdate) VALUES (1, 1, 1, now());
+insert into channel_user(id, user_id, channel_id, regdate) VALUES (2, 2, 1, now());
+insert into channel_user(id, user_id, channel_id, regdate) VALUES (3, 4, 1, now());
 
 insert into user_oauth_info(id,access_token,sub,name,picture,email,user_id)
 VALUES (1,'ya29.GlvXBbqz71mWDrxUVDqC2AiZTPHxHMIdch69Ml9NFeKmr9qfVL9g22QA64slm7wS4fiBqW7x24tbGBrMXlj0AqvStYEvPmQMYc4r7V4XgM_R-y9wOoBg-ZVKKss3','105547134406045421901','soso','https://lh3.googleusercontent.com/-EpeCRDESczo/AAAAAAAAAAI/AAAAAAAAA2A/dnUWLQwBFh4/photo.jpg','noriming2@gmail.com',1);
