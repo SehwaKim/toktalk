9ce37dd2d4f4a51e44dcee0da9142429f71da955
serena <noriming2@gmail.com>
2018-06-24 03:54:01 +0900

    update : remember-me를 위한 설정 추가, 접근권한 hasAnyRole()로 변경

--- src/main/java/com/chat/toktalk/config/WebApplicationSecurityConfig.java
+++ src/main/java/com/chat/toktalk/config/WebApplicationSecurityConfig.java
@@ -9,21 +9,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.Filter;
+import javax.sql.DataSource;
 
 @Configuration
 public class WebApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     private final Filter filter;
 
-
-    @Autowired
-    TokTalkUserDetailsService tokTalkUserDetailsService;
     public WebApplicationSecurityConfig(Filter googleFilter) {
         this.filter = googleFilter;
     }
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    TokTalkUserDetailsService tokTalkUserDetailsService;
 
 
     @Override
@@ -41,26 +47,34 @@ public class WebApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/users/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/api/**").permitAll()
-                .anyRequest().fullyAuthenticated()
+                .anyRequest().hasAnyRole("ADMIN", "USER")
                 .and().headers().frameOptions().disable()
-                .and()
-                    .formLogin()
-                        .loginProcessingUrl("/users/login")
-                        .loginPage("/users/login")
-                        .usernameParameter("email")
-                        .passwordParameter("password")
-                        .successHandler(userAuthenticationSuccessHandler())
-                //.and().rememberMe().rememberMeParameter("remember-me")
-              //  .failureHandler(new UserAuthenticationFailureHandler())
-                .and()
-                    .csrf().disable()
-                    .addFilterBefore(new AlreadyLoginCheckFilter(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(filter, BasicAuthenticationFilter.class);
+                .and().formLogin()
+                .loginProcessingUrl("/users/login")
+                .loginPage("/users/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and().rememberMe()
+                .rememberMeCookieName("remember-me")
+                .userDetailsService(tokTalkUserDetailsService)
+                .tokenValiditySeconds(24 * 60 * 60) //1day
+                .tokenRepository(persistentTokenRepository())
+                .and().csrf()
+                .disable()
+                .addFilterBefore(new AlreadyLoginCheckFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(filter, BasicAuthenticationFilter.class);
     }
 
 
-     @Bean
-     public UserAuthenticationSuccessHandler userAuthenticationSuccessHandler(){
-         return new UserAuthenticationSuccessHandler();
-     }
+    @Bean
+    public UserAuthenticationSuccessHandler userAuthenticationSuccessHandler() {
+        return new UserAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
+    }
 }
