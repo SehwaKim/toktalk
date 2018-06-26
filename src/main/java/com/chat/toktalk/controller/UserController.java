ad014debfaf829dfef0836e528f5eeb55c11e106
serena <noriming2@gmail.com>
2018-06-27 05:19:48 +0900

    update:  signup을 위한 path 추가

--- src/main/java/com/chat/toktalk/controller/UserController.java
+++ src/main/java/com/chat/toktalk/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.chat.toktalk.controller;
 
 import com.chat.toktalk.domain.User;
+import com.chat.toktalk.dto.UserForm;
 import com.chat.toktalk.security.LoginUserInfo;
 import com.chat.toktalk.service.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@Log4j2
 @Controller
 @RequestMapping("/users")
 public class UserController {
@@ -31,5 +37,34 @@ public class UserController {
             returnValue = "not_empty";
         }
         return returnValue;
+    }
+
+    @GetMapping("/signup")
+    public String signUpForm(){
+        return "users/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signUp(@Valid UserForm userForm, BindingResult bindingResult){
+        log.info(userForm.getEmail());
+        if(bindingResult.hasErrors()){
+            return "users/signup";
+        }
+        //이미 존재하는 이메일인지 확인하기
+
+        return "redirect:/";
+    }
+
+    public String findPassword(){
+
+        return null;
+    }
+
+    public String disConnectSocial(){
+        return null;
+    }
+
+    public String cancleUserAccount(){
+        return null;
     }
 }
--- src/main/java/com/chat/toktalk/dto/UserForm.java
+++ src/main/java/com/chat/toktalk/dto/UserForm.java
@@ -1,4 +1,16 @@
 package com.chat.toktalk.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
 public class UserForm {
+    @NotBlank
+    String email;
+    @NotBlank
+    String name;
+    @NotBlank
+    String password;
 }
