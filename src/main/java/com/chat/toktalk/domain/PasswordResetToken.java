7fa02ac1d52569b356cdce53cd89d067c8b31eda
serena <noriming2@gmail.com>
2018-07-10 15:44:26 +0900

    update: 패스워드 찾기 기능을 위해 이메일로 보낼 Token저장을 위한 Entity추가

--- src/main/java/com/chat/toktalk/domain/PasswordResetToken.java
+++ src/main/java/com/chat/toktalk/domain/PasswordResetToken.java
@@ -1,4 +1,38 @@
 package com.chat.toktalk.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.Date;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "password_reset_token")
 public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_id")
+    Long tokenId;
+
+    String token;
+
+    @Column(name = "expiry_date")
+    Date expiryDate;
+
+
+    public void setExpiryDate(int minutes){
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.MINUTE,minutes);
+        this.expiryDate = now.getTime();
+    }
+
+    public boolean isExpired(){
+        return new Date().after(this.expiryDate);
+    }
+
+
 }
