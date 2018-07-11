0ae3e6b741da83076a58be47164dd134c1f9abb6
serena <noriming2@gmail.com>
2018-07-12 01:01:42 +0900

    update: 탈퇴상태가 아닐때 존재하는 유저 조건 추가

--- src/main/java/com/chat/toktalk/validator/UserValidator.java
+++ src/main/java/com/chat/toktalk/validator/UserValidator.java
@@ -1,18 +1,22 @@
 package com.chat.toktalk.validator;
 
 import com.chat.toktalk.domain.User;
+import com.chat.toktalk.domain.UserStatus;
 import com.chat.toktalk.service.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+@Log4j2
 @Component
 public class UserValidator implements Validator {
 
     @Autowired
     UserService userService;
+
     @Override
     public boolean supports(Class<?> aClass) {
         return User.class.equals(aClass);
@@ -21,12 +25,13 @@ public class UserValidator implements Validator {
     @Override
     public void validate(Object object, Errors errors) {
         User user = (User)object;
+        User existingUser = userService.getUserByEmail(user.getEmail());
 
         if(user.getNickname().length() < 2 || user.getNickname().length() > 8){
             errors.rejectValue("nickname","required","2이상 8이하의 글자를 입력 해주세요");
         }
 
-        if(userService.getUserByEmail(user.getEmail()) != null){
+        if(existingUser != null && !UserStatus.DELETE.equals(existingUser.getUserStatus())){
             errors.rejectValue("email","required","이미 존재하는 계정 입니다.");
         }
 
