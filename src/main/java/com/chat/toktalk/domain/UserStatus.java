1369661c6fce2dbb763e78bb76388dd413750772
serena <noriming2@gmail.com>
2018-08-06 08:48:49 +0900

    update: Oauth상태 추가

--- src/main/java/com/chat/toktalk/domain/UserStatus.java
+++ src/main/java/com/chat/toktalk/domain/UserStatus.java
@@ -1,5 +1,24 @@
 package com.chat.toktalk.domain;
 
 public enum UserStatus {
-    NORMAL,DELETE,OAUTH
+    NORMAL("normal"),DELETE("delete"),OAUTH("oauth");
+    private String attribute;
+
+    UserStatus(String attribute) {
+        this.attribute = attribute;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+
+
+    @Override
+    public String toString() {
+        return attribute;
+    }
 }
