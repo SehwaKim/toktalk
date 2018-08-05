4ef7de181f72ed1647cd63f7744fa0c39088b83c
serena <noriming2@gmail.com>
2018-08-06 08:39:57 +0900

    update: 사용자 화면 변경으로 인한 resources 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into user (id, email, password, nickname, regdate ,user_status) VALUES (1, 'test', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '아이스베어', now(),'NORMAL');
-insert into user (id, email, password, nickname, regdate ,user_status) VALUES (2, 'piakatie@naver.com', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '갈색곰', now(),'NORMAL');
+insert into user (id, email, password, nickname, regdate ,user_status) VALUES (1, 'noriming2@gmail.com', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '아이스베어', now(),'NORMAL');
+insert into user (id, email, password, nickname, regdate ,user_status) VALUES (2, 'yeonjeong.serena@gmail.com', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '갈색곰', now(),'NORMAL');
 insert into user (id, email, password, nickname, regdate ,user_status) VALUES (3, 'test3', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '판다', now(),'NORMAL');
 insert into user (id, email, password, nickname, regdate ,delete_date ,user_status) VALUES (4, 'bee@naver.com', '{bcrypt}$2a$10$qS48/8nM2fSagy1di.whF.tutE/VZ9/wwOkGBcm.Ty8mOKLfwpv/G', '땡벌', now(),now(),'DELETE');
 
@@ -12,5 +12,5 @@ insert into user_roles(id, role_state, user_id) VALUES (5, 'USER', 4);
 insert into channel(id, name, type, regdate) VALUES (1, 'general', 'public', now());
 insert into channel_user(id, user_id, channel_id, first_read_id, regdate) VALUES (1, 1, 1, 1, now());
 
-insert into user_oauth_info(id,access_token,sub,name,picture,email,user_id)
+insert into oauth_info(id,access_token,sub,name,picture,email,user_id)
 VALUES (1,'ya29.GlvXBbqz71mWDrxUVDqC2AiZTPHxHMIdch69Ml9NFeKmr9qfVL9g22QA64slm7wS4fiBqW7x24tbGBrMXlj0AqvStYEvPmQMYc4r7V4XgM_R-y9wOoBg-ZVKKss3','105547134406045421901','soso','https://lh3.googleusercontent.com/-EpeCRDESczo/AAAAAAAAAAI/AAAAAAAAA2A/dnUWLQwBFh4/photo.jpg','noriming2@gmail.com',1);
--- src/main/resources/static/js/user-account.js
+++ src/main/resources/static/js/user-account.js
@@ -1,7 +1,85 @@
-// $(document).ready(function () {
-//     $('html, body').animate({
-//         scrollTop: $('#address').offset().top
-//     }, 'slow');
-// });
+
+window.onload = function () {
+
+    //google버튼 클릭시.
+    $("#google").on("click", function () {
+        $.ajax({
+            url: '/users/check-status',
+            type: 'POST',
+            success: function (data) {
+                if ($("#google").hasClass("btn-google")) {
+                    //연결되어 있지않은 상태.
+                    showConfirmBox("소셜 연결을 사용하시겠습니까?", "/login/google");
+
+                } else if ($("#google").hasClass("btn-outline-google")) {
+                    //연결되어 있지 않은 상태
+
+                    if (data == "oauth") {
+                        showConfirmBox("소셜 연결을 해지 하시려면 패스워드 변경을 먼저 하셔야 합니다. 진행 하시겠습니까?", "/identity/password/forgot");
+
+                    } else {
+                        showConfirmBox("소셜 연결을 해지 하시겠습니까?", "/users/disconnect");
+                    }
+                }
+            }
+        });
+    });
+
+    //google 버튼 레이아웃 결정.
+    $.ajax({
+        url: '/users/check-oauth',
+        type: 'POST',
+        success: function (isEmptyOauth) {
+
+            if (isEmptyOauth == false) {
+                $('#google').attr("value", "연결끊기");
+                $("#google").removeClass("btn-google");
+                $("#google").addClass("btn-outline-google");
+            } else if (isEmptyOauth == true) {
+                $('#google').attr("value", "연결하기");
+                $("#google").addClass("btn-google");
+                $("#google").removeClass("btn-outline-google");
+            }
+        }
+    });
+
+}
 
 
+function progressBar() {
+    var dialog;
+    //프로그래스바 진행
+    $.ajax({
+        url: '/identity/password/sent',
+        type: 'GET',
+        beforeSend: function () {
+            dialog = bootbox.dialog({
+                message: '<p class="text-center">이메일을 보내는 중입니다. 잠시만 기다려 주세요.<br><i class="fa fa-spin fa-spinner"/></p>',
+                closeButton: false
+            });
+        },
+        success: function () {
+            dialog.modal('hide');
+        }
+    });
+
+}
+
+function showConfirmBox(message, path) {
+    bootbox.confirm({
+        message: message,
+        buttons: {
+            confirm: {
+                label: '확인',
+                className: 'alert-no'
+            },
+            cancel: {
+                label: '취소',
+                className: 'alert-yes'
+            }
+        },
+        callback: function (result) {
+            location.href = path;
+        }
+    });
+}
